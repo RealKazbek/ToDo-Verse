02d76897a8ea281e62e1971e2c168ce1c5325b81
Kazbek <37765@iitu.edu.kz>
2025-05-28 13:29:08 +0500

    Added and created postgreSql database with prisma

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "./lib/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
